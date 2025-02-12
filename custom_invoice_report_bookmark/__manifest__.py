e232f2213ae32c9fe1a9b65bcbcd66949e279d0e
youseff-dow-group <youssef@dowgroup.com>
2025-02-12 12:09:05 +0200

    Add files via upload

---
+++ custom_invoice_report_bookmark/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Custom Invoice Report Module",
+    "summary": """ """,
+    "version": "0.1",
+    "license": "LGPL-3",
+    "author": "",
+    "website": "",
+    "depends": ['sale'],
+    "data": [
+        'data/report_format.xml',
+        'report/report_template.xml',
+        'report/template.xml',
+
+    ],
+
+
+}
